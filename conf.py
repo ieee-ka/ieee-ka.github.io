a1c6a17a965c03d01ad6b383f510b7073dd1e11f
Sebastian Koslowski <koslowski@kit.edu>
2016-07-06 14:18:04 +0200

    add impressum

--- conf.py
+++ conf.py
@@ -789,7 +789,8 @@ FEED_LINKS_APPEND_QUERY = False
 
 # A HTML fragment describing the license, for the sidebar.
 # (translatable)
-LICENSE = ""
+LICENSE = """
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/80x15.png" /></a>"""
 # I recommend using the Creative Commons' wizard:
 # https://creativecommons.org/choose/
 # LICENSE = """
@@ -800,7 +801,11 @@ LICENSE = ""
 
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
-CONTENT_FOOTER = 'Contents &copy; {date}         <a href="mailto:{email}">{author}</a> - Powered by         <a href="https://getnikola.com" rel="nofollow">Nikola</a>         {license}'
+CONTENT_FOOTER = (
+	'Contents &copy; {date} <a href="mailto:{email}">{author}</a> {license}'
+	' - Powered by <a href="https://getnikola.com" rel="nofollow">Nikola</a>'
+	'<br /><a href="{impressum}">Impressum</a>'
+)
 
 # Things that will be passed to CONTENT_FOOTER.format().  This is done
 # for translatability, as dicts are not formattable.  Nikola will
@@ -820,7 +825,8 @@ CONTENT_FOOTER_FORMATS = {
             "email": BLOG_EMAIL,
             "author": BLOG_AUTHOR,
             "date": time.gmtime().tm_year,
-            "license": LICENSE
+            "license": LICENSE,
+            "impressum": "/impressum",
         }
     )
 }
