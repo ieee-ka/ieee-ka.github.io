114e803308457e51ad5ea4222f554a69908830a7
Felix Wunsch <felix.wunsch@kit.edu>
2016-10-17 09:35:01 +0200

    Nikola auto commit.
    Nikola version: 7.7.9

--- conf.py
+++ conf.py
@@ -135,7 +135,7 @@ NAVIGATION_LINKS = {
         ((
             ("/calendar/", "Kalender"),
             ("/events/student-paper-contest/", "Student Paper Contest"),
-            ("/events/sigint-callenge/", "Signal Intelligence Callenge"),
+            ("/events/sigint-challenge/", "Signal Intelligence Callenge"),
         ), 'Events'),
         ("/archive.html", "Archiv"),
         ("/tags/", "Tags"),
